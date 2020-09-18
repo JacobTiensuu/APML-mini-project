8bb8657b5dcbe98ceef61ab8d10cad74ba905255
Jacob Tiensuu <jacob.tiensuu@gmail.com>
2020-09-18 15:28:35 +0200

    Added plotting and shuffling for Q5

--- Q5_ADF.py
+++ Q5_ADF.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import Q4_gibbsSampler
 
-def ADF(mu_start, sigma_start, sigma_t, num_samples, burn_in):
+def ADF(mu_start, sigma_start, sigma_t, num_samples, burn_in, shuffle):
 
     # Read data from csv file and remove date and time stamp and draws
     data = pd.read_csv('SerieA.csv')
@@ -13,6 +13,10 @@ def ADF(mu_start, sigma_start, sigma_t, num_samples, burn_in):
 
     data = data.drop(labels = ['yyyy-mm-dd', 'HH:MM'], axis = 1)
     data = data.drop(data.index[i])
+
+    # Shuffle data
+    if (shuffle == 1):
+        data = data.sample(frac=1)
     
     team1 = data.team1.unique()
     skills = np.ones(len(team1))*mu_start
@@ -55,19 +59,28 @@ def ADF(mu_start, sigma_start, sigma_t, num_samples, burn_in):
         teams.at[teams['Name']==team1, 'rank'] = mu_1-3*sigma_1
         teams.at[teams['Name']==team2, 'rank'] = mu_2-3*sigma_2
 
+    # Plotting data
+    Name = teams['Name'].to_numpy()
+    rank = teams['rank'].to_numpy()
+    plt.figure(1)
+    plt.bar(Name,rank)
+    plt.show()
 
     # Sorting the dataframe
     teams = teams.sort_values(by='rank', ascending=False)
     print(teams)
     return pred_true, pred_false
 
+
+    
 def main():
     mu_start = 10
     sigma_start = 10/3
     sigma_t = 1 
-    num_samples = 300
-    burn_in = 100
-    ADF(mu_start, sigma_start, sigma_t, num_samples, burn_in)
+    num_samples = 50
+    burn_in = 10
+    shuffle = 1
+    ADF(mu_start, sigma_start, sigma_t, num_samples, burn_in, shuffle)
 
 
 if __name__ == "__main__":
