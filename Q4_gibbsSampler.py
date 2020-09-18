Fixed an error when using sign and added some variables for the gibbs sampler to return. Fixed some problems with versions for Q4

Q4_gibbsSampler.py
@@ -2,6 +2,8 @@
 import scipy.stats as stats
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+
 
 def gibbs_sampler(mu_1, mu_2, sigma_1, sigma_2, sigma_t, y, num_samples, burn_in):
     # Number of samples k
@@ -15,15 +17,17 @@ def gibbs_sampler(mu_1, mu_2, sigma_1, sigma_2, sigma_t, y, num_samples, burn_in
     # Set initial values
     s_1[0] = np.random.normal(mu_1, sigma_1)
     s_2[0] = np.random.normal(mu_2, sigma_2)
+
+ 
     
     for i in range(burn_in+k-1):
         mu_t = s_1[i] - s_2[i]
         if(y == 1):
-            a = 0
+            a = (0-(mu_t))/sigma_t
             b = np.infty
         else:
             a = -np.infty
-            b = 0
+            b = (0-(mu_t))/sigma_t
         # Calculate the output   
         out[i+1] = stats.truncnorm.rvs(a,b,mu_t,sigma_t)
         # Get sigma and Mu from the posterior to calculate the new prior
@@ -36,8 +40,14 @@ def gibbs_sampler(mu_1, mu_2, sigma_1, sigma_2, sigma_t, y, num_samples, burn_in
     # Discard burn-in samples
     s_1 = s_1[burn_in:-1]
     s_2 = s_2[burn_in:-1]
+    mu_1 = np.mean(s_1)
+    mu_2 = np.mean(s_2)
+    var_1 = np.var(s_1)
+    var_2 = np.var(s_2)
     
-    return s_1, s_2, out
+    return s_1, s_2, mu_1, mu_2, var_1, var_2
+
+# Calculate
 
 def main():
     # Test the gibbs sampler
@@ -48,12 +58,16 @@ def main():
     sigma_t = 5
     y = 1
     num_samples = 200
-    burn_in = 100
-    s1,s2,out = gibbs_sampler(mu_1, mu_2, sigma_1, sigma_2, sigma_t, y, num_samples, burn_in)
+    burn_in = 0
+    s1,s2, mu_1, mu_2, var_1, var_2 = gibbs_sampler(mu_1, mu_2, sigma_1, sigma_2, sigma_t, y, num_samples, burn_in)
 
     # Plot figure
     plt.figure(1)
     plt.plot(s1)
-    plt.show()
+    plt.show()  
+
+if __name__ == "__main__":
+    main()
+
 
         

Q5_ADF.py
@@ -14,7 +14,6 @@ def ADF(mu_start, sigma_start, sigma_t, num_samples, burn_in):
     data = data.drop(labels = ['yyyy-mm-dd', 'HH:MM'], axis = 1)
     data = data.drop(data.index[i])
     
-
     team1 = data.team1.unique()
     skills = np.ones(len(team1))*mu_start
     vars = np.ones(len(team1))*sigma_start
@@ -38,10 +37,10 @@ def ADF(mu_start, sigma_start, sigma_t, num_samples, burn_in):
         mu_2 = teams.loc[teams['Name'] == team2, 'skill'].iat[0]
         sigma_2 = teams.loc[teams['Name'] == team2, 'variance'].iat[0]
         t = data.loc[i, 'score1'] - data.loc[i, 'score2']
-        p = sign(mu_1-mu_2) #for Q6
         y = np.sign(t)
 
         #Check predictions for Q6
+        p = np.sign(mu_1-mu_2) #for Q6
         if(p == y):
             pred_true = pred_true + 1
         else: